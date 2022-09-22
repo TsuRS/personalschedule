e0e570285194a3d7e3ee873d1925b699043974c4
TsuRS <109035435+TsuRS@users.noreply.github.com>
2022-09-23 00:53:09 +0900

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,83 @@
+class Elements
+{
+    static GetHeadings()
+    {
+        return ["Week", "Mon", "Tue", "Wed", "Thu", "Fri"];
+    }
+
+    static GetProperties()
+    {
+        return ["week", "mon", "tue", "wed", "thu", "fri"];
+    }
+
+    static GetElements()
+    {
+        return [
+        {
+            week: 1,
+            mon: "数学 HL",
+            tue: "地理",
+            wed: "",
+            thu: "英語",
+            fri: "英語"
+        },
+        {
+            week: 2,
+            mon: "数学 HL",
+            tue: "物理 HL",
+            wed: "日本語",
+            thu: "英語",
+            fri: "地理"
+        },
+        {
+            week: 3,
+            mon: "ESS",
+            tue: "物理 HL",
+            wed: "日本語",
+            thu: "数学 SL",
+            fri: "日本語"
+        },
+        {
+            week: 4,
+            mon: "ESS",
+            tue: "経済",
+            wed: "体育",
+            thu: "物理 SL",
+            fri: "数学 SL"
+        },
+        {
+            week: 5,
+            mon: "物理 HL",
+            tue: "",
+            wed: "英語",
+            thu: "物理 SL",
+            fri: ""
+        },
+        {
+            week: 6,
+            mon: "体育",
+            tue: "",
+            wed: "TOK",
+            thu: "",
+            fri: "ESS"
+        },
+        {
+            week: 7,
+            mon: "LHR",
+            tue: "数学 SL",
+            wed: "TOK",
+            thu: "体育",
+            fri: "経済"
+        },
+        {
+            week: "Extra",
+            mon: "",
+            tue: "英語",
+            wed: "経済",
+            thu: "",
+            fri: ""
+        },
+
+    ]
+    }
+}
---
+++ startup.js
@@ -0,0 +1,72 @@
+
+window.onload = function()
+{
+    createTable2();
+    currentTime();
+}
+
+function createTable2()
+{
+    const currentDate = new Date();
+    const anchorOff = 4
+    const anchorDate = new Date(2022, 6, 3, 0, 0, 0, 0);
+    const weekDifference = (currentDate.getTime() - anchorDate.getTime())/(1000*3600*24*7);
+    const weekOff = Math.floor((weekDifference + anchorOff) % 7);
+
+    const elements = Elements.GetElements();
+    const headings = Elements.GetHeadings();
+    const properties = Elements.GetProperties();
+
+    // create table
+    const table = document.createElement("table");
+
+    // create row for headings...
+    const hrow = document.createElement("tr");
+    table.appendChild(hrow);
+
+    // ...and add cells to it
+    for(let heading of headings)
+    {
+        const th = document.createElement("th");
+        const thtext = document.createTextNode(heading);
+        th.appendChild(thtext);
+        hrow.appendChild(th);
+    }
+
+    // iterate data, adding rows and cells for each item
+    for(let element of elements)
+    {
+        if(element.week !== weekOff )
+        {
+        const drow = document.createElement("tr");
+        table.appendChild(drow);
+
+        for(let property of properties)
+        {
+            const td = document.createElement("td");
+            const tdtext = document.createTextNode(element[property]);
+            td.appendChild(tdtext);
+            drow.appendChild(td);
+        }
+    }
+    }
+
+    // add table to div
+    document.getElementById("tablediv").appendChild(table);
+}
+
+
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+  
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+      
+    let time = hh + ":" + mm;
+  
+    document.getElementById("clock").innerText = time; 
+    var t = setTimeout(function(){ currentTime() }, 1000); 
+  
+  }
