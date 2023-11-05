b754f9b2f971491804cb3912b8f1177264c3f1ce
TsuRS <109035435+TsuRS@users.noreply.github.com>
2023-11-05 20:06:14 +0900

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,73 @@
+class Elements
+{
+    static GetHeadings()
+    {
+        return ["☆", "Mon", "Tue", "Wed", "Thu", "Fri"];
+    }
+
+    static GetProperties()
+    {
+        return ["week", "mon", "tue", "wed", "thu", "fri"];
+    }
+    static GetElements()
+    {
+        return [
+        {
+            week: 1,
+            mon: "英語",
+            tue: "日本語",
+            wed: "ESS",
+            thu: "数学 SL",
+            fri: ""
+        },
+        {
+            week: 2,
+            mon: "英語",
+            tue: "日本語",
+            wed: "物理 HL",
+            thu: "",
+            fri: ""
+        },
+        {
+            week: 3,
+            mon: "",
+            tue: "数学 SL",
+            wed: "英語",
+            thu: "日本語",
+            fri: "数学 HL"
+        },
+        {
+            week: 4,
+            mon: "体育",
+            tue: "物理 HL",
+            wed: "英語",
+            thu: "",
+            fri: "物理 SL"
+        },
+        {
+            week: 5,
+            mon: "情報",
+            tue: "ESS",
+            wed: "経済",
+            thu: "体育",
+            fri: "物理 SL"
+        },
+        {
+            week: 6,
+            mon: "物理 SL",
+            tue: "ESS",
+            wed: "数学 SL",
+            thu: "TOK",
+            fri: "英語"
+        },
+        {
+            week: 7,
+            mon: "保険",
+            tue: "経済",
+            wed: "数学 HL",
+            thu: "TOK",
+            fri: "経済"
+        },
+    ]
+    }
+}
---
+++ startup.js
@@ -0,0 +1,83 @@
+
+window.onload = function()
+{
+    createTable2();
+    currentTime();
+}
+
+function createTable2()
+{
+    const currentDate = new Date();
+    const anchorOff = 0
+    const anchorDate = new Date(2022, 6, 3, 0, 0, 0, 0);
+    const weekDifference = (currentDate.getTime() - anchorDate.getTime())/(1000*3600*24*7);
+    const weekOff = Math.floor((weekDifference + anchorOff) % 7);
+
+    const elements = Elements.GetElements();
+    const headings = Elements.GetHeadings();
+    const properties = Elements.GetProperties();
+
+    // create table
+    const table = document.createElement("table");
+
+    // create row for headings...
+    const hrow = document.createElement("tr");
+    table.appendChild(hrow);
+
+    // ...and add cells to it
+    for(let heading of headings)
+    {
+        const th = document.createElement("th");
+        const thtext = document.createTextNode(heading);
+        th.appendChild(thtext);
+        hrow.appendChild(th);
+        if(((currentDate.getDay() !== 6) && (currentDate.getDay() !== 0)) && (heading == (headings[currentDate.getDay()]))) 
+        {
+            th.classList.add("active")
+        }
+    }
+
+    // iterate data, adding rows and cells for each item
+    for(let element of elements)
+    {
+        if(element.week !== weekOff)
+        {
+            if(element.week !== weekOff + 7 )
+            {
+                const drow = document.createElement("tr");
+                table.appendChild(drow);
+
+                for(let property of properties)
+                {
+                    const td = document.createElement("td");
+                    const tdtext = document.createTextNode(element[property]);
+                    td.appendChild(tdtext);
+                    drow.appendChild(td);
+                    if(((currentDate.getDay() !== 6) && (currentDate.getDay() !== 0)) && (property == (properties[currentDate.getDay()]))) 
+                    {
+                        td.classList.add("active")
+                    }
+                }
+            }
+        }
+    }
+
+    // add table to div
+    document.getElementById("tablediv").appendChild(table);
+}
+
+
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+  
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+      
+    let time = hh + ":" + mm;
+  
+    document.getElementById("clock").innerText = time; 
+    var t = setTimeout(function(){ currentTime() }, 1000); 
+  
+  }
